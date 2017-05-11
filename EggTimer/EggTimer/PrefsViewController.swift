d8c9d8afb127b6c4b8935644ef6664de10ca4769
Cyrille Calvino <calvinocyrille@gmail.com>
2017-05-11 22:40:34 +0200

    Menu and preferences created

---
+++ EggTimer/EggTimer/PrefsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PrefsViewController.swift
+//  EggTimer
+//
+//  Created by Cyrille Calvino on 11/05/2017.
+//  Copyright © 2017 Cyrille Calvino. All rights reserved.
+//
+
+import Cocoa
+
+class PrefsViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
